0b301ad9f8b66e5bd50447fc96ff871af0dad3f7
kominsuk1064 <kms106418@gmail.com>
2026-03-09 22:58:22 +0900

    solve: W02_problem-solving_파이썬문법-최댓값

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,13 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+arr = []
+
+for _ in range(9):
+    arr.append(int(input()))
+
+max_value = max(arr)
+index = arr.index(max_value) + 1
+
+print(max_value)
+print(index)
