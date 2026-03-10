7fa2ac16707f2c158d0aeebf49c675c3737c065a
kominsuk1064 <kms106418@gmail.com>
2026-03-10 17:21:30 +0900

    solve: W02_problem-solving_문자열_단어공부

--- week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
+++ week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
@@ -1,2 +1,18 @@
 # 문자열 - 단어 공부 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/1157
+
+word = input().upper()
+
+alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+cnt = []
+
+for a in alphabets:
+    cnt.append(word.count(a))
+
+max_value = max(cnt)
+
+if cnt.count(max_value) > 1:
+    print("?")
+else:
+    i = cnt.index(max_value)
+    print(alphabets[i])
