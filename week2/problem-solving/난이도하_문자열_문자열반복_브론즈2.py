73388f94a71c522b053f5bb9f92253da54415de1
kominsuk1064 <kms106418@gmail.com>
2026-03-10 16:59:39 +0900

    solve: W02_problem-solving_문자열_문자열반복

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,15 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+
+t = int(input())
+
+for _ in range(t):
+    r, s = input().split()
+    r = int(r)
+
+    result = ""
+
+    for alph in s:
+        result += alph * r
+
+    print(result)
