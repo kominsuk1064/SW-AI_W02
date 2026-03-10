dd559a7bdc0569cc2143023934eae759b5b014da
kominsuk1064 <kms106418@gmail.com>
2026-03-10 10:15:08 +0900

    solve: W02_problem-solving_정수론_소수찾기

--- week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
+++ week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
@@ -1,2 +1,24 @@
 # 정수론 - 소수 찾기 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/1978
+
+
+n = int(input())
+nums = list(map(int, input().split()))
+
+count = 0
+
+for num in nums:
+    if num < 2:
+        continue
+    
+    is_prime = True
+    
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+    
+    if is_prime:
+        count += 1
+
+print(count)
