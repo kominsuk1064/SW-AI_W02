6ff33c1d3aeaa05cbf50546b73bc166f66daaa18
kominsuk1064 <kms106418@gmail.com>
2026-03-10 20:53:07 +0900

    solve: W02_problem-solving_문자열_IPv6

--- week2/problem-solving/난이도중_문자열_IPv6_실버1.py
+++ week2/problem-solving/난이도중_문자열_IPv6_실버1.py
@@ -1,2 +1,29 @@
 # 문자열 - IPv6 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/3107
+
+
+add = input()
+
+if "::" in add:
+    left, right = add.split("::")
+
+    if left:
+        left_part = left.split(":")
+    else:
+        left_part = []
+
+    if right:
+        right_part = right.split(":")
+    else:
+        right_part = []
+
+    miss = 8 - (len(left_part)) + (len(right_part))
+
+    part = left_part + ["0000"] * miss + right_part
+
+else:
+    part = add.split(":")
+
+part = [p.zfill(4) for p in part]
+
+print(":".join(part))
