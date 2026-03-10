d3647bb20ee35c52d67c821052526c721ea9d0d6
kominsuk1064 <kms106418@gmail.com>
2026-03-10 13:50:50 +0900

    solve: W02_problem-solving_배열_평균은넘겠지

--- week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
+++ week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
@@ -1,2 +1,21 @@
 # 배열 - 평균은 넘겠지 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/4344
+
+c = int(input())
+
+for _ in range(c):
+    data = list(map(int, input().split()))
+
+    n = data[0]
+    scores = data[1:]
+
+    average = sum(scores) / n
+
+    count = 0
+    for score in scores:
+        if score > average:
+            count += 1
+
+    ratio = (count / n) * 100
+
+    print(f"{ratio:.3f}%")
