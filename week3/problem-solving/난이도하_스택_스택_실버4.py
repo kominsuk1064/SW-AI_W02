4cd411e6f099187ff5d94694a4927e9f25cf5c40
kominsuk1064 <kms106418@gmail.com>
2026-03-14 11:47:08 +0900

    solve: week3_problem-solving_스택_스택

--- week3/problem-solving/난이도하_스택_스택_실버4.py
+++ week3/problem-solving/난이도하_스택_스택_실버4.py
@@ -1,2 +1,35 @@
 # 스택 - 스택 (백준 실버 4)
 # 문제 링크: https://www.acmicpc.net/problem/10828
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+stack = []
+
+for _ in range(N):
+    command = input().split()
+
+    if command[0] == "push":
+        stack.append(int(command[1]))
+
+    elif command[0] == "pop":
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop())
+
+    elif command[0] == "size":
+        print(len(stack))
+
+    elif command[0] == "empty":
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+    
+    elif command[0] == "top":    
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
