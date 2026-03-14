3742d3c4707f671da23ec40035a425cdc46b5b43
kominsuk1064 <kms106418@gmail.com>
2026-03-14 15:47:26 +0900

    solve: week3_problem-solving_큐_카드2

--- week3/problem-solving/난이도하_큐_카드2_실버4.py
+++ week3/problem-solving/난이도하_큐_카드2_실버4.py
@@ -1,2 +1,14 @@
 # 큐 - 카드2 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/2164
+
+from collections import deque
+
+N = int(input())
+
+card = deque(range(1, N + 1))
+
+while len(card) > 1:
+    card.popleft()
+    card.append(card.popleft())
+
+print (card[0])
