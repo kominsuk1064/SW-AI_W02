d4037aa458879d9ae343095f9cbc146a706910b3
kominsuk1064 <kms106418@gmail.com>
2026-03-14 16:50:35 +0900

    solve: week3_problem-solving_해시테이블_민균이의비밀번호

--- week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
+++ week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
@@ -1,2 +1,14 @@
 # 해시 테이블 - 민균이의 비밀번호 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/9933
+
+N = int(input())
+
+passwords = []
+
+for _ in range(N):
+    passwords.append(input())
+
+for c in passwords:
+    if c[::-1] in passwords:
+        print(len(c), c[len(c)//2])
+        break
