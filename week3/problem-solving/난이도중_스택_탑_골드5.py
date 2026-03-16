d1d0bcf70a3a6a27cb152bec2a3ce84c4b1d17e9
kominsuk1064 <kms106418@gmail.com>
2026-03-16 17:03:54 +0900

    solve: week3_problem-solving_스택_탑

--- week3/problem-solving/난이도중_스택_탑_골드5.py
+++ week3/problem-solving/난이도중_스택_탑_골드5.py
@@ -1,2 +1,21 @@
 # 스택 - 탑 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2493
+
+N = int(input())
+height = list(map(int, input().split()))
+
+stack = []
+answer = []
+
+for i in range(N):
+    while stack and stack[-1][1] < height[i]:
+        stack.pop()
+
+    if not stack:
+        answer.append(0)
+    else:
+        answer.append(stack[-1][0])
+
+    stack.append((i + 1, height[i]))
+
+print(*answer)
