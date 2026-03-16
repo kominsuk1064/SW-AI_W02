1fca64c1ad869c9f0d481b7cc605592446c23e65
kominsuk1064 <kms106418@gmail.com>
2026-03-16 15:08:16 +0900

    solve: week3_problem-solving_분할정복_곱셈

--- week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
+++ week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
@@ -1,2 +1,16 @@
 # 분할정복 - 곱셈 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1629
+
+def multiplication(A, B, C):
+    if B == 1:
+        return A % C
+    
+    division = multiplication(A, B // 2, C)
+
+    if B % 2 == 0:
+        return (division * division) % C
+    else:
+        return (division * division * A) % C
+
+A, B, C = map(int, input().split())
+print(multiplication(A, B, C))
