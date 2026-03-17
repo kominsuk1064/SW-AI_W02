7f650c5e44d539fc0163f6c6dea9a69cb1bef918
kominsuk1064 <kms106418@gmail.com>
2026-03-17 15:34:31 +0900

    solve: week3_problem-solving_해시테이블_세수의합

--- week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
+++ week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
@@ -1,2 +1,24 @@
 # 해시 테이블 - 세 수의 합 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/2295
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+U = [int(input()) for _ in range(N)]
+
+U.sort()
+
+absum = set()
+
+for i in range(N):
+    for j in range(N):
+        absum.add(U[i] + U[j])
+
+for i in range(N-1, -1, -1):
+    d = U[i]
+    for j in range(N):
+        c= U[j]
+        if d - c in absum:
+            print(d)
+            exit()
