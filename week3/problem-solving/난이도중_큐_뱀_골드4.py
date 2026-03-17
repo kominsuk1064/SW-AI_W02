8339360bbbb813dd9c953db179bd9cbaae7cde26
kominsuk1064 <kms106418@gmail.com>
2026-03-17 19:31:06 +0900

    solve: week3_problem-solving_분할정복_색종이만들기

--- week3/problem-solving/난이도중_큐_뱀_골드4.py
+++ week3/problem-solving/난이도중_큐_뱀_골드4.py
@@ -1,56 +1,64 @@
 # 큐 - 뱀 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/3190
 
-from collections import deque
+from collections import deque   # 양쪽에서 빠르게 삽입/삭제 가능한 deque 사용
 
-N = int(input())
-K = int(input())
+N = int(input())    # 보드 크기
+K = int(input())    # 사과 개수
 
-board = [[0] * N for _ in range(N)]
+board = [[0] * N for _ in range(N)]     # 보드 생성(0: 빈칸, 1: 사과)
 
-for _ in range(K):
+for _ in range(K):          # 사과 위치 입력
     a, b = map(int, input().split())
     board[a-1][b-1] = 1
 
-L = int(input())
+L = int(input())    # 방향 전환 횟수
 
-turns = {}
+turns = {}              # 시간별 방향 전환 정보 저장
 for _ in range(L):
     X, C = input().split()
-    turns[int(X)] = C
+    turns[int(X)] = C   # X초 후 C 방향으로 회전   
 
+# 방향(오른쪽, 아래 왼쪽, 위)
 row = [0, 1, 0, -1]
 columns = [1, 0, -1, 0]
-direction = 0
+direction = 0   # 처음 방향 오른쪽
 
 snake = deque()
-snake.append((0, 0))
+snake.append((0, 0))            # 뱀 시작 위치
 
 time = 0
-head_row, head_columns = 0, 0
+head_row, head_columns = 0, 0   # 뱀 머리 위치
 
-while True:
-    time += 1
+while True:     # 매 초마다 이동
+    time += 1   # 시간 증가
+
+    # 다음 위치 계산(현재 방향 기준)
     now_row = head_row + row[direction]
     now_columns = head_columns + columns[direction]
 
+    # 벽에 부딪히면 게임 종료
     if not (0 <= now_row < N and 0 <= now_columns < N):
         break
 
+    # 자기 몸에 부딪히면 게임 종료
     if (now_row, now_columns) in snake:
         break
 
+    # 머리를 새로운 위치로 이동
     snake.append((now_row, now_columns))
 
+    # 사과가 있는 경우 꼬리 유지
     if board[now_row][now_columns] == 1:
         board[now_row][now_columns] = 0
-    else:
+    else:   # 사과가 없는 경우 꼬리 제거
         snake.popleft()
 
-    head_row, head_columns = now_row, now_columns
+    head_row, head_columns = now_row, now_columns   # 머리 위치 업데이트
 
+    # 방향 전환 시간이면 방향 변경
     if time in turns:
-        if turns[time] == 'D':
+        if turns[time] == 'D':      # 오른쪽 화면
             direction = (direction + 1) % 4
         else:
             direction = (direction - 1) % 4
--- week3/problem-solving/난이도하_분할정복_색종이만들기_실버2.py
+++ week3/problem-solving/난이도하_분할정복_색종이만들기_실버2.py
@@ -1,2 +1,37 @@
 # 분할정복 - 색종이 만들기 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/2630
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+paper =  [list(map(int, input().split())) for _ in range(N)]
+
+white = 0
+blue = 0
+
+def origami(x, y, size):
+    global white, blue
+
+    color = paper[x][y]
+
+    for i in range(x, x + size):
+        for j in range(y, y + size):
+            if paper[i][j] != color:
+                half = size // 2
+                origami(x, y, half)
+                origami(x, y + half, half)
+                origami(x + half, y, half)
+                origami(x + half, y + half, half)
+                return
+
+    if color == 0:
+        white += 1
+    else:
+        blue += 1
+
+origami(0, 0, N)
+
+print(white)
+print(blue)
