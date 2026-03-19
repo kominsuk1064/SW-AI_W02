cbadabbd66205d7c791c6fab4a6a0c57da3ead0a
kominsuk1064 <kms106418@gmail.com>
2026-03-19 21:36:09 +0900

    solve: week2_problem-solving_완전탐색_차이를최대로

--- week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
+++ week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
@@ -1,2 +1,17 @@
 # 완전탐색 - 차이를 최대로 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/10819
+
+from itertools import permutations
+
+N = int(input())
+A = list(map(int, input().split()))
+
+answer = 0
+
+for p in permutations(A):
+    total = 0
+    for i in range(N - 1):
+        total += abs(p[i] - p[i + 1])
+    answer = max(answer, total)
+
+print(answer)
