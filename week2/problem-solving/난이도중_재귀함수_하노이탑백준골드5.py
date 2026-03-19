d75d422f28bb0d2fc4a3f9a8d2efa4c07f221a5f
kominsuk1064 <kms106418@gmail.com>
2026-03-19 22:36:02 +0900

    solve: week2_problem-solving_재귀함수_하노이탑

--- week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
+++ week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
@@ -1,2 +1,24 @@
 # 재귀함수 - 하노이 탑 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/1914
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+print((1 << N) - 1)
+
+if N <= 20:
+    result = []
+
+    def hanoi(n, first, second, third):
+        if n == 1:
+            result.append(f"{first} {third}")
+            return
+
+        hanoi(n - 1, first, third, second)
+        result.append(f"{first} {third}")
+        hanoi(n - 1, second, first, third)
+
+    hanoi(N, 1, 2, 3)
+    print("\n".join(result))
