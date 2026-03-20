42401caf36c6a02795b00d6cf49f2e069190fe64
kominsuk1064 <kms106418@gmail.com>
2026-03-20 23:20:20 +0900

    solve: week4_basic_04_bfs

--- week4/basic/04_bfs.py
+++ week4/basic/04_bfs.py
@@ -47,15 +47,23 @@ def bfs(graph, start):
     
     # TODO: 큐 생성 및 시작 정점 추가
     ## 방문한 정점 집합
-    pass
+    seen = set([start])
+    queue = deque([start])
 
     # TODO: 큐가 빌 때까지 반복
     ## 큐에서 정점 꺼내기
     ## 인접한 정점들 확인
     ## 방문하지 않은 정점이면 큐에 추가
-    pass
+    while queue:
+        vertex = queue.popleft()
+        visited.append(vertex)
+
+        for adjacent in graph[vertex]:
+            if adjacent not in seen:
+                seen.add(adjacent)
+                queue.append(adjacent)
     
-    return visited
+    return list(visited)
 
 # 테스트 케이스
 if __name__ == "__main__":
