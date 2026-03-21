d5a5dbea54ce495ceea3a67b486671b145e3e291
kominsuk1064 <kms106418@gmail.com>
2026-03-21 15:31:07 +0900

    solve: week3_problem-solving_트리_트리만들기

--- week4/problem-solving/난이도하_트리_트리만들기_실버4.py
+++ week4/problem-solving/난이도하_트리_트리만들기_실버4.py
@@ -1,2 +1,12 @@
 # 트리 - 트리 만들기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/14244
+
+n, m = map(int, input().split())
+
+k = n - m + 2
+
+for i in range(k - 1):
+    print(i, i + 1)
+
+for i in range(k, n):
+    print(1, i)
