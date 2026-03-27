c1dd932b69e064ba22323deb84cd995ca89b73ab
kominsuk1064 <kms106418@gmail.com>
2026-03-27 20:31:16 +0900

    solve: week4_problem-solving_ 그래프DFSBFS_바이러스

--- week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
@@ -1,2 +1,24 @@
 # 그래프, DFS, BFS - 바이러스 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/2606
+
+n = int(input())
+m = int(input())
+
+graph = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def dfs(v):
+    visited[v] = True
+    
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(i)
+
+dfs(1)
+
+print(sum(visited) - 1)
