e60ee6f8b403779239d98ac7ad8d1fea32ca6660
kominsuk1064 <kms106418@gmail.com>
2026-03-27 20:31:14 +0900

    solve: week4_problem-solving_ 그래프DFSBFS_점프왕쩰리

--- week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
@@ -1,2 +1,34 @@
 # 그래프, DFS, BFS - 점프왕 쩰리 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/16173
+
+from collections import deque
+
+N = int(input())
+board = [list(map(int, input().split())) for _ in range(N)]
+
+visited = [[False] * N for _ in range(N)]
+queue = deque()
+queue.append((0, 0))
+visited[0][0] = True
+
+while queue:
+    r, c = queue.popleft()
+
+    if board[r][c] == -1:
+        print("HaruHaru")
+        break
+
+    jump = board[r][c]
+
+    nr, nc = r + jump, c
+    if 0 <= nr < N and 0 <= nc < N and not visited[nr][nc]:
+        visited[nr][nc] = True
+        queue.append((nr, nc))
+
+    nr, nc = r, c + jump
+    if 0 <= nr < N and 0 <= nc < N and not visited[nr][nc]:
+        visited[nr][nc] = True
+        queue.append((nr, nc))
+
+else:
+    print("Hing")
