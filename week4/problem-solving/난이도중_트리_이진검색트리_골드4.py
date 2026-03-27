d9c3cb6c6fc5ba78d256155dff19e2cf5846522c
kominsuk1064 <kms106418@gmail.com>
2026-03-27 20:31:27 +0900

    solve: week4_problem-solving_트리_이진검색트리

--- week4/problem-solving/난이도중_트리_이진검색트리_골드4.py
+++ week4/problem-solving/난이도중_트리_이진검색트리_골드4.py
@@ -1,2 +1,27 @@
 # 트리 - 이진 검색 트리 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/5639
+
+import sys
+sys.setrecursionlimit(10**6)
+
+preorder = []
+for line in sys.stdin:
+    preorder.append(int(line.strip()))
+
+def postorder(start, end):
+    if start > end:
+        return
+
+    root = preorder[start]
+
+    mid = end + 1
+    for i in range(start + 1, end + 1):
+        if preorder[i] > root:
+            mid = i
+            break
+
+    postorder(start + 1, mid - 1)
+    postorder(mid, end)
+    print(root)
+
+postorder(0, len(preorder) - 1)
