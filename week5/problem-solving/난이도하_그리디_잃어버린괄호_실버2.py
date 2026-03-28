cef2f6bf4c5d9f26f1e4c2e88e197e2752f8e1a9
kominsuk1064 <kms106418@gmail.com>
2026-03-28 20:33:53 +0900

    solve: week5_problem-solving_그리디_잃어버린괄호

--- week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
+++ week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
@@ -1,2 +1,13 @@
 # 그리디 - 잃어버린 괄호 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1541
+
+expression = input()
+
+minus = expression.split('-')
+
+result = sum(map(int, minus[0].split('+')))
+
+for i in minus[1:]:
+    result -= sum(map(int, i.split("+")))
+
+print(result)
