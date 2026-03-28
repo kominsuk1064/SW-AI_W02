d35e71cab984502002efebef3f38702616005053
kominsuk1064 <kms106418@gmail.com>
2026-03-28 20:07:48 +0900

    solve: week5_problem-solving_그리디_동전0

--- week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -1,2 +1,16 @@
 # 그리디 - 동전 0 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/11047
+
+N, K = map(int, input().split())
+
+coins = []
+for _ in range(N):
+    coins.append(int(input()))
+
+count = 0
+
+for coin in reversed(coins):
+    count += K // coin
+    K %= coin
+
+print(count)
