48cd05647660bda4a2988940d38e88915f2a6bc0
kominsuk1064 <kms106418@gmail.com>
2026-03-28 21:41:17 +0900

    solve: week5_problem-solving_DP_동전

--- week5/problem-solving/난이도중_DP_동전_골드5.py
+++ week5/problem-solving/난이도중_DP_동전_골드5.py
@@ -1,2 +1,18 @@
 # DP - 동전 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9084
+
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    coins = list(map(int, input().split()))
+    M = int(input())
+
+    dp = [0] * (M + 1)
+    dp[0] = 1
+
+    for coin in coins:
+        for i in range(coin, M + 1):
+            dp[i] += dp[i - coin]
+
+    print(dp[M])
