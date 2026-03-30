089475e0c4de40e68aebee9aee58729647b38cf9
kominsuk1064 <kms106418@gmail.com>
2026-03-30 20:36:30 +0900

    solve: week5_problem-solving_그리디_신입사원

--- week5/problem-solving/난이도중_그리디_신입사원_실버1.py
+++ week5/problem-solving/난이도중_그리디_신입사원_실버1.py
@@ -1,2 +1,23 @@
 # 그리디 - 신입 사원 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1946
+
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    volunteer = [tuple(map(int, input().split())) for _ in range(N)]
+    
+    volunteer.sort()
+    
+    count = 1
+    min_interview = volunteer[0][1]
+    
+    for i in range(1, N):
+        if volunteer[i][1] < min_interview:
+            count += 1
+            min_interview = volunteer[i][1]
+    
+    print(count)
