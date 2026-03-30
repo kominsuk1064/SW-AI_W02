a6c6c589dd4c6ee67822fa33aaef7358e63b52fa
kominsuk1064 <kms106418@gmail.com>
2026-03-30 17:01:30 +0900

    solve: week5_problem-solving_DP_평범한배낭

--- week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
+++ week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
@@ -1,2 +1,14 @@
 # DP - 평범한 배낭 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/12865
+
+N, K = map(int, input().split())
+
+dp = [0] * (K + 1)
+
+for _ in range(N):
+    weight, value = map(int, input().split())
+
+    for i in range(K, weight - 1, -1):
+        dp[i] = max(dp[i], dp[i - weight] + value)
+
+print(dp[K])
