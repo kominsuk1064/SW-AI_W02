e5e91351e683a39e70f9547b2a35e08928a6de90
kominsuk1064 <kms106418@gmail.com>
2026-03-31 17:51:13 +0900

    solve: week5_problem-solving_그리디_회의실배정

--- week5/problem-solving/난이도중_그리디_회의실배정_골드5.py
+++ week5/problem-solving/난이도중_그리디_회의실배정_골드5.py
@@ -1,2 +1,17 @@
 # 그리디 - 회의실 배정 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/1931
+
+N = int(input())
+meetings = [tuple(map(int, input().split())) for _ in range(N)]
+
+meetings.sort(key=lambda x: (x[1], x[0]))
+
+count = 0
+last_end = 0
+
+for start, end in meetings:
+    if start >= last_end:
+        count += 1
+        last_end = end
+
+print(count)
